27b68d48d6c5025cf929e6d7a276ad01313bb4d1
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-30 10:07:54 +0500

    feat: add MongoDB connection using mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (req, res) => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connect successfully");
+    } catch (error) {
+        console.log(`Error: ,${error.message}`)
+    }
+}
