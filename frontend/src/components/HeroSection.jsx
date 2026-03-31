1a1cec6926faf051e1d6d490e291fe514c9f552b
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-31 14:59:33 +0500

    feat: add styled hero section with responsive layout and typography

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='flex flex-col justify-center gap-6 items-center h-[80vh] bg-[#003F3A] p-4'>
+            <h1 className='text-6xl sm:text-6xl font-extrabold text-center  text-[#14ead8]'>Track Visitors with One Script</h1>
+            <h2 className='text-white text-center text-sm px-10'>Simple, fast, and developer-friendly analytics. No complex setup. No repeated integration.</h2>
+        </div>
+    )
+}
+
+export default HeroSection
