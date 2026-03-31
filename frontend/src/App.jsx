4fbfdef0ddd4ccab399240041a88a9134045a845
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-31 15:02:16 +0500

    import LandingPage in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import LandingPage from './pages/LandingPage'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-2xl text-red-500'>Hello World!</h1>
+      <LandingPage/>
     </div>
   )
 }
