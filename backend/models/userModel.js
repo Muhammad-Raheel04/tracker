16ef797af5cfa75063ac1016c7fee26623844bbe
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-07 15:00:32 +0500

    add user schema with authentication fields

---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const userScheme = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    token: {
+        type: String,
+        default: null
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isLoggedIn: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true })
+
+export default mongoose.model('User', userScheme);
