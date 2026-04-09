a760d521f6c39d2580837ae93b2da29c88287ea0
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-09 19:28:40 +0500

    add header what we deliver and footer components

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import WhatWeDeliver from '../components/WhatWeDeliver'
 
 const LandingPage = () => {
   return (
     <div>
+      <Header/>
       <HeroSection/>
+      <WhatWeDeliver/>
+      <Footer/>
     </div>
   )
 }
