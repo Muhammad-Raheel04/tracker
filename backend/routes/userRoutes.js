7c601554d8e53f6972035437ca6deafe24d98938
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-09 23:02:36 +0500

    integrate verify route in user routes for making a post request for email verification

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, verify } from '../controllers/userController.js';
 const router=express.Router();
 
 router.post('/register',registerUser)
-
+router.post('/verify',verify);
 export default router;
