628031af5c2af4ce452f3b06f76ce00770d11ce5
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-10 12:29:29 +0500

    feat: add login route to user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { registerUser, verify } from '../controllers/userController.js';
+import { login, registerUser, verify } from '../controllers/userController.js';
 const router=express.Router();
 
 router.post('/register',registerUser)
 router.post('/verify',verify);
+router.post('/login',login);
 export default router;
