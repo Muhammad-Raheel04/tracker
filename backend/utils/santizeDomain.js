ded6c24bd46f2c039f92b3611a8918e20e1b719c
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-12 23:25:32 +0500

    improve domain sanitization by trimming and handling trailing slashes

--- backend/utils/santizeDomain.js
+++ backend/utils/santizeDomain.js
@@ -1,7 +1,8 @@
 export const sanitizeDomain=(domain)=>{
     return domain
+        .trim()
         .toLowerCase()
         .replace(/https?:\/\//,'')
         .replace(/^www\./,'')
-        .replace(/\/$/,'')
+        .replace(/\/+$/,'')
 }
