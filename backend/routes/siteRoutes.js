3cc96986bebed3ae1c5306d9a7d819a423a83347
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-14 23:34:21 +0500

    feat: add isAuthenticated middleware to site register endpoint for securing it

--- backend/routes/siteRoutes.js
+++ backend/routes/siteRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getVersion, registerSite } from '../controllers/siteController.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
 
 const router=express.Router();
 
 router.get('/version',getVersion);
-router.post('/register',registerSite);
+router.post('/register',isAuthenticated,registerSite);
 
 export default router;
