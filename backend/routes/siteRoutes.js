ab23539671279332d5e73210c92c603a2deb9403
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-19 18:21:40 +0500

    fix(siteRoutes.js): remove route for get version endpoint

--- backend/routes/siteRoutes.js
+++ backend/routes/siteRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getScript, getVersion, registerSite, verifySite } from '../controllers/siteController.js';
+import { getScript, registerSite, verifySite } from '../controllers/siteController.js';
 import { isAuthenticated } from '../middleware/isAuthenticated.js';
 
 const router=express.Router();
 
-router.get('/version',getVersion);
+
 router.post('/register',isAuthenticated,registerSite);
 router.post('/verify/:siteId',isAuthenticated,verifySite);
 router.get('/script/:siteId',isAuthenticated,getScript);
