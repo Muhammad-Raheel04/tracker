9797ce63a3969f187d8dbed3dc24ede60fab5304
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-19 08:13:31 +0500

    feat: add refresh token route for access token renewal

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { login, registerUser, verify } from '../controllers/authController.js';
+import { login, refreshToken, registerUser, verify } from '../controllers/authController.js';
 const router=express.Router();
 
 router.post('/register',registerUser)
 router.post('/verify',verify);
 router.post('/login',login);
+router.post('/refresh',refreshToken);
 export default router;
