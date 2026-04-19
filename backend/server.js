3b4571113d4c206a7d72d983f2a60b07dee79082
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-19 20:19:14 +0500

    fix(server): add cors middleware apply it globally

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,13 @@ import healthRoutes from './routes/healthRoutes.js';
 const app=express();
 const PORT=process.env.PORT;
 
+app.use(cors({
+    origin:'*',
+}));
 app.use(express.json())
 app.use('/api/v1/health',healthRoutes);
 app.use('/api/v1/sites',siteRoutes);
-app.use('/api/v1/analytics',pageVisitRoutes)
+app.use('/api/v1/visits',pageVisitRoutes)
 app.use('/api/v1/auth',authRoutes)
 app.listen(PORT,()=>{
     console.log(`tracker listening at http://localhost:${PORT}`)
