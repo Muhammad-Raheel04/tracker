7ffa8087cc701e9b600a05af8bc09adb1a80c4d3
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-19 09:09:45 +0500

    fix: updated auth route naming incosistencies

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import pageVisitRoutes from './routes/pageVisitRoutes.js';
 import siteRoutes from './routes/siteRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 const app=express();
 const PORT=process.env.PORT;
 
 app.use(express.json())
 app.use('/api/v1/sites',siteRoutes);
 app.use('/api/v1/analytics',pageVisitRoutes)
-app.use('/api/v1/users',userRoutes)
+app.use('/api/v1/auth',authRoutes)
 app.listen(PORT,()=>{
     console.log(`tracker listening at http://localhost:${PORT}`)
     connectDB();
