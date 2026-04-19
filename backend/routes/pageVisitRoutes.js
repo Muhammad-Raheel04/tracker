a61d78ba4d30b7a679531b73d9d6d253446e1d91
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-19 18:21:06 +0500

    feat(pageVisitRoutes.js): add route for get version endpoint

--- backend/routes/pageVisitRoutes.js
+++ backend/routes/pageVisitRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import {trackPageVisits,} from '../controllers/analyticsController.js';
+import { getVersion } from '../controllers/siteController.js';
 const router=express.Router();
 
 router.post('/page-visit',trackPageVisits);
-
+router.get('/version',getVersion);
 export default router;
