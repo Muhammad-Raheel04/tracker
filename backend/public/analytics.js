076b2aad22d4b37cb753bf013776fbd3836cb1cb
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-21 15:10:41 +0500

    fix(analytics.js): add missing analytics in script URL

--- backend/public/analytics.js
+++ backend/public/analytics.js
@@ -3,7 +3,7 @@
     .then(r=>r.json())
     .then(({version})=>{
         const script=document.createElement('script')
-        script.src=`https://tracker-production-25f3.up.railway.app/api/v1/analytics.v${version}.js`;
+        script.src=`https://tracker-production-25f3.up.railway.app/api/v1/analytics/analytics.v${version}.js`;
         script.defer=true;
         document.head.appendChild(script);
     })
