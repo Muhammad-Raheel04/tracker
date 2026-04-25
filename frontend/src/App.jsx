6036ef6a2d38030ebe44821941ed85d65dcaa1aa
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-25 21:46:05 +0500

    feat(App): add routing using createBrowser and RouterProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './App.css'
 import LandingPage from './pages/LandingPage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AnalyticsPage from './pages/AnalyticsPage';
 
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<><LandingPage></LandingPage></>
+  },
+  {
+    path:"/analytics",
+    element:<><AnalyticsPage></AnalyticsPage></>
+  },
+])
 const App = () => {
   return (
     <div>
-      <LandingPage/>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   )
 }
