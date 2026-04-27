9580d13f8a639d5a552934bd636d766541ca370c
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-27 15:19:29 +0500

    fix(server): allow all domains to make calls to server

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ import cors from 'cors';
 const app=express();
 const PORT=process.env.PORT;
 
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true,
-}));
+app.use(cors());
 app.use(express.json())
 app.use('/api/v1/health',healthRoutes);
 app.use('/api/v1/sites',siteRoutes);
