3024022d2c14f6c1abed80f1a415a6c9c54a5861
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-27 10:17:44 +0500

    feat(main): configure Toaster from react-hot-toaster and define styles for success and error messages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+import {Toaster} from 'react-hot-toast'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <App />
+      <Toaster 
+      position='bottom-right' 
+      reverseOrder={false}
+      toastOptions={{
+        style:{
+          background:"#042f2b",
+          color:"#05fce8",
+          padding:"12px 16px",
+          borderRadius:"10px",
+        },
+        success:{
+          style:{
+            background:"#042f2b",
+          }
+        },
+        error:{
+          style:{
+            background:"#042f2b",
+          }
+        },
+        duration:3000,
+      }}
+      ></Toaster>
   </StrictMode>,
 )
