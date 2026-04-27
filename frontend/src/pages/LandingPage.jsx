2bb6b03af626c22d2ac16ea3312214df7c277014
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-27 11:03:22 +0500

    chore(LandingPage): remove unnecessary imports and related sections from landing LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import WhatWeDeliver from '../components/WhatWeDeliver'
-import IntegrationSection from '../components/IntegrationSection'
 
 const LandingPage = () => {
   return (
     <div>
       <Header/>
       <HeroSection/>
-      <WhatWeDeliver/>
-      <IntegrationSection/>
       <Footer/>
     </div>
   )
