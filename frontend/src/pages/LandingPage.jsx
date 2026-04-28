01273f5ef2d3d62bed6f52107bf06484f89c3c0e
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-28 06:21:49 +0500

    feat(LandingPage): integrate Features section

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Features from '../components/Features'
 
 const LandingPage = () => {
   return (
     <div>
       <Header/>
       <HeroSection/>
+      <Features/>
       <Footer/>
     </div>
   )
