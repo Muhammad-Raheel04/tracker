56859a84216f2c2d42226253f4093700e21d3cdf
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-29 18:11:49 +0500

    feat(siteController): add deleteSite controller to delete a site using siteId received from params

--- backend/controllers/siteController.js
+++ backend/controllers/siteController.js
@@ -178,3 +178,29 @@ export const getAllSites=async(req,res)=>{
         })
     }
 }
+export const deleteSite=async(req,res)=>{
+    try{
+        const {siteId}=req.params;
+
+        const site=await Site.findByIdAndDelete(siteId);
+        
+        if(!site){
+            return res.status(404).json({
+                success:false,
+                message:"Site not found",
+            })
+        }
+        
+
+        return res.status(200).json({
+            success:true,
+            message:`${site.name} deleted successfully`,
+        })
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+            error:error?.message
+        })
+    }
+}
