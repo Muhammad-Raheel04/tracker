a4dcb7586dedec4741e73e27c8ad89d28edd8797
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-30 23:32:02 +0500

    feat(analyticsRoutes): integrate getAnalytics controller at /:siteId get end point and add isAuthenticated middleware also

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getVersion, serveLoader, serveTrackerScript, trackPageVisits } from '../controllers/analyticsController.js';
+import { getAnalytics, getVersion, serveLoader, serveTrackerScript, trackPageVisits } from '../controllers/analyticsController.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
 const router=express.Router();
 
 router.get('/version',getVersion);
 router.get('/analytics.js',serveLoader);
 router.get('/analytics.v:version.js',serveTrackerScript);
+router.get('/:siteId',isAuthenticated,getAnalytics)
 router.post('/track',trackPageVisits);
 
 export default router;
