918393e1c3e214b53c09c46e44d4905f1d633cc8
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-03 10:25:46 +0500

    feat(authRoutes): add protected /logout endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, refreshToken, registerUser, verify } from '../controllers/authController.js';
+import { login, logout, refreshToken, registerUser, verify } from '../controllers/authController.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
 const router=express.Router();
 
 router.post('/register',registerUser)
 router.post('/verify',verify);
 router.post('/login',login);
 router.post('/refresh',refreshToken);
+router.post('/logout',isAuthenticated,logout);
 export default router;
