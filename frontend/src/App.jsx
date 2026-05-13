9930bd830929b679a5c6acd7e8c8268994ffcfb2
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-13 23:12:14 +0500

    feat(App): add routes for verify and verify email pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,8 @@ import AddSite from './pages/dashboard/AddSite';
 import Overview from './pages/dashboard/Overview';
 import Analytics from './pages/dashboard/Analytics';
 import ProtectRoute from './components/ProtectRoute';
+import Verify from './pages/Verify';
+import VerifyEmail from './pages/VerifyEmail';
 const router=createBrowserRouter([
   {
     path:"/",
@@ -22,6 +24,14 @@ const router=createBrowserRouter([
   {
     path:"/login",
     element:<><Login></Login></>
+  },
+  {
+    path:"/verify",
+    element:<><Verify></Verify></>
+  },
+  {
+    path:"/verify/:token",
+    element:<><VerifyEmail></VerifyEmail></>
   },
   {
     path:"/dashboard",
